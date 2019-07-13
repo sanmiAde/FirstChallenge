8428667537aa5d853e756da06a39065dc20ebf8b
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2019-07-13 17:52:29 +0100

    implemented about screen.

---
+++ app/src/main/java/com/sanmiaderibigbe/firstchallenge/AboutALCActivity.java
@@ -0,0 +1,57 @@
+package com.sanmiaderibigbe.firstchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutALCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+
+        getSupportActionBar().setTitle("About ALC");
+
+        WebView webView  = findViewById(R.id.web_view);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+
+        webView.loadUrl("https://andela.com/alc/");
+
+
+    }
+
+    public static Intent newAboutALCActivity(Context context) {
+        return  new Intent(context, AboutALCActivity.class);
+    }
+}
--- app/src/main/java/com/sanmiaderibigbe/firstchallenge/MainActivity.java
+++ app/src/main/java/com/sanmiaderibigbe/firstchallenge/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +20,20 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(getString(R.string.main_title_bar_text));
 
         Button myProfileBtn = findViewById(R.id.btn_my_profile);
+        Button aboutALCBtn = findViewById(R.id.btn_about_alc);
 
         myProfileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = ProfileActivity.newProfileActivity(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+
+        aboutALCBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = AboutALCActivity.newAboutALCActivity(MainActivity.this);
                 startActivity(intent);
             }
         });
