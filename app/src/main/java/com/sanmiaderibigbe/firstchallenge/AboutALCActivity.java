ab7f5a80b89a636068be50f3ff89b2e98c953149
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2019-07-14 13:12:53 +0100

    Completed project.

--- app/src/main/java/com/sanmiaderibigbe/firstchallenge/AboutALCActivity.java
+++ app/src/main/java/com/sanmiaderibigbe/firstchallenge/AboutALCActivity.java
@@ -6,13 +6,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class AboutALCActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private  WebView webView;
+
+    public static Intent newAboutALCActivity(Context context) {
+        return  new Intent(context, AboutALCActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +30,21 @@ public class AboutALCActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("About ALC");
 
-        WebView webView  = findViewById(R.id.web_view);
+
+        progressBar = findViewById(R.id.web_view_progressbar);
+        webView = findViewById(R.id.web_view);
+
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setLoadWithOverviewMode(true);
         settings.setUseWideViewPort(true);
+        initLoadingScreen();
+        initWebViewListener(webView);
 
+
+    }
+
+    private void initWebViewListener(WebView webView) {
         webView.setWebViewClient(new WebViewClient() {
 
             @Override
@@ -35,23 +53,33 @@ public class AboutALCActivity extends AppCompatActivity {
             }
 
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                Toast.makeText(AboutALCActivity.this, description, Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 return super.shouldOverrideUrlLoading(view, request);
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
+                terminateLoadingScreen();
                 super.onPageFinished(view, url);
             }
         });
 
         webView.loadUrl("https://andela.com/alc/");
-
-
     }
 
-    public static Intent newAboutALCActivity(Context context) {
-        return  new Intent(context, AboutALCActivity.class);
+    private void initLoadingScreen(){
+        progressBar.setVisibility(View.VISIBLE);
+        webView.setVisibility(View.INVISIBLE);
+    }
+
+    private void terminateLoadingScreen(){
+        progressBar.setVisibility(View.INVISIBLE);
+        webView.setVisibility(View.VISIBLE);
     }
 }
